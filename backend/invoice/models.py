f847884bae751164aff102c244f1225f8f2f6ca0
Hamid Hosseini <info.husseini@gmail.com>
2024-09-07 17:53:54 +0330

    index.html page made

--- backend/invoice/models.py
+++ backend/invoice/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+import random
+import string
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Invoice(models.Model):
+    def random_unique_code():
+        return ''.join(random.choices(string.ascii_uppercase + string.digits, k=45))
+    
+    unique_code = models.CharField(max_length=45, unique=True, default=random_unique_code)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
